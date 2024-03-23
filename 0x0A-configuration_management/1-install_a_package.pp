6c84984d8e3075fba81b478fba7bd485365d3a05
Kufre Akpan <contactkufreakpan@gmail.com>
2024-03-23 00:14:23 -0700

    install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 #!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
+# Install an especific version of flask (2.1.1)
 package {'flask':
-  ensure   => '2.1.0',
+  ensure   => '2.1.1',
   provider => 'pip3'
 }
