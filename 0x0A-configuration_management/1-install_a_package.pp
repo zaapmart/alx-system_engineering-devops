afcd8ff639716783ee46183f8c88714a32331b10
Kufre Akpan <contactkufreakpan@gmail.com>
2024-03-23 00:36:02 -0700

    install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 #!/usr/bin/pup
-# Install a specific version of flask (2.0.1)
-# Install a specific version of Werkzeug (2.0.2)
+# Install an especific version of flask (2.1.0)
+# Install an especific version of Werkzeug (2.1.1)
 package {'Werkzeug':
-  ensure   => '2.0.2',
+  ensure   => '2.1.1',
   provider => 'pip3'
 }
